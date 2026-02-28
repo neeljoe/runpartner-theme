ab78459a7a87ee772a85b38c975cf0f3d66b733e
neeljoe <joe.neel@gmail.com>
2026-02-28 11:14:58 +0530

    functions php file added

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php 
+<?php
+function fictional_university_child_styles() {
+    wp_enqueue_style(
+        'fictional-university-child-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'fictional_university_child_styles' );
