5e515aeccfd309a579f1a265c6efc35286d8c197
neeljoe <joe.neel@gmail.com>
2026-03-14 13:51:03 +0530

    image not sorted

--- patterns/hero-card.php
+++ patterns/hero-card.php
@@ -1,14 +1,8 @@
-<?php
-/**
- * Title: Hero Card Grid
- * Slug: runpartner/hero-card
- * Categories: runpartner
- * Description: Hero card display
- */
-?>
+<!-- wp:template-part {"slug":"header","theme":"runpartner-theme","area":"header"} /-->
+
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"url":"http://runpartner.local/wp-content/uploads/2026/03/heroimage-1024x576.jpg","id":44,"dimRatio":50,"customOverlayColor":"#373630","isUserOverlayColor":false,"contentPosition":"top center","sizeSlug":"large","align":"full","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull has-custom-content-position is-position-top-center"><img class="wp-block-cover__image-background wp-image-44 size-large" alt="" src="http://runpartner.local/wp-content/uploads/2026/03/heroimage-1024x576.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#373630"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"47px"} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"url":"http://runpartner.local/wp-content/uploads/2026/03/heroimage-1024x576.webp","id":186,"dimRatio":50,"customOverlayColor":"#363630","isUserOverlayColor":false,"contentPosition":"top center","sizeSlug":"large","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-top-center"><img class="wp-block-cover__image-background wp-image-186 size-large" alt="" src="http://runpartner.local/wp-content/uploads/2026/03/heroimage-1024x576.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#363630"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"47px"} -->
 <div style="height:47px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
@@ -22,12 +16,58 @@
 
 <!-- wp:buttons {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://runpartner.local/marathons/">Browse Marathons</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/marathons">Browse Marathons</a></div>
 <!-- /wp:button -->
 
 <!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Runner Tools</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/tools">Runner Tools</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"textAlign":"center","align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"var:preset|spacing|50","bottom":"0"}}}} -->
+<h2 class="wp-block-heading alignwide has-text-align-center" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:0;padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">Marathons</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:query {"queryId":0,"query":{"perPage":50,"pages":0,"offset":0,"postType":"marathon","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"align":"wide"} -->
+<div class="wp-block-query alignwide"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-5"}}}},"backgroundColor":"accent-11","textColor":"accent-5","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-accent-5-color has-accent-11-background-color has-text-color has-background has-link-color"><!-- wp:post-featured-image {"isLink":true,"sizeSlug":"large"} /-->
+
+<!-- wp:post-title {"textAlign":"center","isLink":true,"fontSize":"large"} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|30"},"padding":{"bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|40"},"elements":{"link":{"color":{"text":"#faf9f2eb"}}},"color":{"text":"#faf9f2eb"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group has-text-color has-link-color" style="color:#faf9f2eb;margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"blockGap":"3px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"runpartner/acf","args":{"field":"location"}}}},"fontSize":"small"} -->
+<p class="has-small-font-size"></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>,</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"runpartner/acf","args":{"field":"country"}}}},"fontSize":"small"} -->
+<p class="has-small-font-size"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"3px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">First held  - </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"runpartner/acf","args":{"field":"first_edition_year"}}}},"fontSize":"small"} -->
+<p class="has-small-font-size"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","theme":"runpartner-theme","area":"footer"} /-->
