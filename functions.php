fbdff8cc6b3e9589b9c0d1c185d9666799078c04
gracydev31-ship-it <gracydev31@gmail.com>
2026-05-16 23:16:38 +0530

    front page started

--- functions.php
+++ functions.php
@@ -9,6 +9,21 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+// Support featured-events-query className on core/query blocks
+// Injects meta_key/meta_value to filter events by _rp_event_featured = 1
+add_filter('query_loop_block_query_vars', 'runpartner_featured_events_query', 10, 2);
+function runpartner_featured_events_query(array $query, WP_Block $block): array {
+    $attrs = $block->parsed_block['attrs'] ?? [];
+    $class_name = $attrs['className'] ?? '';
+
+    if (strpos($class_name, 'featured-events-query') !== false) {
+        $query['meta_key'] = '_rp_event_featured';
+        $query['meta_value'] = '1';
+    }
+
+    return $query;
+}
+
 // Enqueue theme stylesheet
 add_action('wp_enqueue_scripts', 'runpartner_enqueue_styles');
 function runpartner_enqueue_styles() {
